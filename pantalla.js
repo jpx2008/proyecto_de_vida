775a35b5bf5b103b83f5bb88329ad004209086f3
jpx2008 <javier.estuardo.pineda13@gmail.com>
2026-04-08 18:11:12 -0600

    Refactor loading screen code in pantalla.js

--- pantalla.js
+++ pantalla.js
@@ -3,8 +3,6 @@ window.onload = function () {
   let progreso = 0;
   const barra = document.getElementById("barra");
   const texto = document.querySelector(".texto-carga");
-
-
   // Cambiar textos tipo videojuego
   setTimeout(() => texto.innerText = "Inicializando sistema...", 1000);
   setTimeout(() => texto.innerText = "Cargando datos...", 2000);
@@ -14,10 +12,8 @@ window.onload = function () {
   let intervalo = setInterval(() => {
     progreso += 10;
     barra.style.width = progreso + "%";
-
     if (progreso >= 100) {
       clearInterval(intervalo);
-
       // Oculta pantalla de carga
       document.getElementById("pantalla-carga").style.display = "none";
     }
