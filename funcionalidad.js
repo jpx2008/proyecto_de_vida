f7dbbd7f85d0541dd2b2397f4fa1beb635e48a7e
jpx2008 <javier.estuardo.pineda13@gmail.com>
2026-05-23 08:02:38 -0600

    Update funcionalidad.js

--- funcionalidad.js
+++ funcionalidad.js
@@ -36,7 +36,7 @@ function showInfo(part){
 
         changeImage("capo.png");
 
-        title.innerHTML = "CAPÓ";
+        title.innerHTML = "MISION Y VISION";
 
         description.innerHTML =
         "Visión Ser responsable, profesional y honesto, llegar a tener una empresa de las importantes, buen padre de familia, todo bajo los valores y principios y mi Misión es Ser un universitario comprometido y ayudar en el bienestar social mediante la educación actuando con integridad y responsabilidad.";
@@ -64,7 +64,7 @@ function showInfo(part){
         changeImage("puerta2izq.png");
 
         title.innerHTML =
-        "PUERTA TRASERA";
+        "AREAS DE APOYO";
 
         description.innerHTML =
       "Ciudadana, Espiritual, Sociocultural <br><br> Intelectual, Física, Emocional, Trabajo, Financiera, Familia";
@@ -77,7 +77,7 @@ function showInfo(part){
         changeImage("puerta3dere.png");
         
         title.innerHTML =
-        "PUERTA DERECHA";
+        "OBJETIVOS DE META";
 
         description.innerHTML =
         "Mi visión Ser responsable, profesional y honesto, tener una empresa importante y formar una buena familia con valores y principios Mi Objetivo Graduarme de la universidad, tener mi propio negocio y apoyar a mi familia.Corto plazo (1 año) Aprobar mis estudios, ser responsable y ahorrar dinero.Fortalezas: responsabilidad y honestidad.Debilidades: distracciones y nerviosismo.Principios: respeto y humildad.Mediano plazo (2 a 3 años)Continuar la universidad, obtener experiencia y empezar mi negocio.Fortalezas: compromiso y esfuerzo.Debilidades: inseguridad y poco tiempo.Principios: honestidad y perseverancia.Largo plazo (4 a 5 años)Graduarme, tener una empresa estable y ayudar a mis padres.Fortalezas: liderazgo y motivación.Debilidades: estrés y cansancio.Principios: integridad, disciplina y responsabilidad.";
@@ -91,7 +91,7 @@ function showInfo(part){
         changeImage("puerta4dere.png");
 
         title.innerHTML =
-        "PUERTA TRASERA DERECHA";
+        "ESTRATREGIAS";
 
         description.innerHTML =
         "A corto plazo, mis estrategias son organizar mejor mi tiempo, cumplir con mis responsabilidades y ahorrar dinero para el futuro. También buscaré evitar distracciones, tener más confianza en mí mismo y aprender de mis errores. Para solucionar problemas, pediré consejos, mantendré una actitud positiva y actuaré con responsabilidad.A mediano plazo, continuaré preparándome en la universidad y ahorraré dinero para iniciar mi propio negocio. Además, mejoraré la organización de mi tiempo, controlaré la inseguridad y administraré mejor mi dinero con disciplina y compromiso.A largo plazo, trabajaré con honestidad y responsabilidad, manteniendo siempre mis valores y principios. También aprenderé a manejar el estrés, ser paciente y tomar decisiones correctas para resolver los problemas con madurez y tranquilidad.";
@@ -104,7 +104,7 @@ function showInfo(part){
 
         changeImage("baul.png");
 
-        title.innerHTML = "BAÚL";
+        title.innerHTML = "CRONOGRAMA";
 
         description.innerHTML =
         "01	Aprobar mis estudios universitarios	 Jun2026,  02 Ser más responsable y disciplinado May2026,03	Ahorrar dinero para mi futuro Jun2026, 04 Obtener experiencia laboral Jun2026,05 Continuar la universidad Feb2027,06	Empezar mi negocio propio Sep2028,07 Cierre de Pensum Nov2030,08 Tener una empresa estable Ene2030";
